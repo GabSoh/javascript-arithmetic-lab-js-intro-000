252a3f1ab7a59806693af54143990d558af646df
Gabriel Soh <soh_shq@yahoo.com>
2020-09-01 09:36:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add(x, y){
   return x + y
 }
+function subtract(x, y){
+  return x -= y
+}
+function multiple(x, y){
+  return x *= y
+}
+function divide(x, y){
+  return x /= y
+}
+function increment(n){
+  return n += n
+}
